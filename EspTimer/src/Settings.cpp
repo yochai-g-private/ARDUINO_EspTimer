8ee24e729d481bd4d19b63618094675e32e22b66
yochai-g-private <yochai.glauber@attunity.com>
2021-11-15 11:07:06 +0200

    SSID

--- EspTimer/src/Settings.cpp
+++ EspTimer/src/Settings.cpp
@@ -86,8 +86,8 @@ namespace NYG
         false,                              // use_WIFI
         {   
             WiFiDef("",                 "",             IPAddress()),
-            WiFiDef(HOME_TEST_SSID,          HOME_TEST_SSID_PASS,   IPAddress()),
-            WiFiDef("YochaiG",          HOME_TEST_SSID_PASS,   IPAddress()),
+            WiFiDef(HOME_TEST_SSID,          HOME_WIFI_COMMON_PASS,   IPAddress()),
+            WiFiDef("YochaiG",          HOME_WIFI_COMMON_PASS,   IPAddress()),
         },                                  // WIFI
         "123",                              // WIFI_order
 
